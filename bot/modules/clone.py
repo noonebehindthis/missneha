893f456df8a1e7b6cfd66a67e0bb51413864bbfe
Meher Mankar <84778567+gigameher473@users.noreply.github.com>
2021-12-03 10:07:37 +0530

    Add GDTOT Support
    Now You Can CLone Gdtot Links Together With Archive and leech support.
    
    Files Taken From-https://github.com/anasty17/mirror-leech-telegram-bot

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -9,7 +9,9 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE, download_dict, download_dict_lock, Interval, LOGS_CHATS
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, new_thread
-
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link
+from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot
+from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 
 @new_thread
 def cloneNode(update, context):
@@ -17,6 +19,17 @@ def cloneNode(update, context):
     reply_to = update.message.reply_to_message
     if len(args) > 1:
         link = args[1]
+    else:
+        link = ''
+    gdtot_link = is_gdtot_link(link)
+    if gdtot_link:
+        try:
+            msg = sendMessage(f"Bypassing GDTOT Link.", context.bot, update)
+            link = gdtot(link)
+            deleteMessage(context.bot, msg)
+        except DirectDownloadLinkException as e:
+            return sendMessage(str(e), context.bot, update)
+    if is_gdrive_link(link):
     elif reply_to is not None:
         reply_text = reply_to.text
         link = reply_text.split('\n')[0]
@@ -82,19 +95,15 @@ def cloneNode(update, context):
                 try:
 
                     for i in LOGS_CHATS:
-
                         msg1 = f'<b>File Cloned: </b> <code>{name}</code>\n'
-
                         msg1 += f'<b>By: </b>{uname}\n'
-                        msg1 += f'<b>Logs By PeruNoob</b>'
-
                         bot.sendMessage(chat_id=i, text=msg1, reply_markup=button, parse_mode=ParseMode.HTML)
-
                 except Exception as e:
-
                     LOGGER.warning(e)
+        if gdtot_link:
+            gd.deletefile(link)
     else:
-        sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
+        sendMessage('Provide G-Drive Or Gdtot Shareable Link to Clone.', context.bot, update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand, cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(clone_handler)
